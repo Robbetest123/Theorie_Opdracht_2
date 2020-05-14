b3ffab3c6d2047a8484a9a5dd5f9308ed1d72f27
Robbetest123 <robbecroes@student.vives.be>
2020-05-14 16:49:02 +0200

    Gok_Programma_V2

--- gokken.sh
+++ gokken.sh
@@ -1,38 +1,38 @@
-Random_Getal=$((1 + RANDOM % 10)) 
+Random_Getal=$((1 + RANDOM % 10)) # random getal aanmaken
 
-Aantalgokken=1 
+Aantalgokken=1  # het aantal gokken in een variabele stoppne
 
 echo 'Probeer het willekeurig getal te raden!'
 
 
-while [ $Aantalgokken -le 3 ]; do
+while [ $Aantalgokken -le 3 ]; do 
     echo 'poging nummer #'$Aantalgokken
     
-    read -p "wat is je gok? [n]: " gok
+    read -p "wat is je gok? [n]: " gok # gok opvragen
 
     
-    while [[ ! $gok =~ (^[1-9]$|^10$)  ]]; do
+    while [[ ! $gok =~ (^[1-9]$|^10$)  ]]; do # kijken of de gok voldoet aan de voorwaarden
         read -p 'geen goede input. jouw gok? [n]: ' gok
     done
 
     
-    if [ $gok -eq $Random_Getal ]; then
-        echo 'Correct! het nummer was:' $Random_Getal
+    if [ $gok -eq $Random_Getal ]; then # indien de gok gelijk is aan het random getal
+        echo 'Correct! het nummer was:' $Random_Getal 
         break;
-    else
+    else # indien de gok niet gelijk is aan het random getal
          
-        if [ $gok -lt $Random_Getal ]; then 
+        if [ $gok -lt $Random_Getal ]; then # indien de gok kleiner is dan het random getal
             echo 'Hoger';
-        elif [ $gok -gt $Random_Getal ]; then
+        elif [ $gok -gt $Random_Getal ]; then # indien de gok groter is dzn het random getal
             echo 'Lager';
         fi
     fi
     
     
-    ((Aantalgokken+=1))
+    ((Aantalgokken+=1)) # het aantal gokken + 1 doen
 done
 
-if [ $gok -ne $Random_Getal ]; then
+if [ $gok -ne $Random_Getal ]; then # indien de laatste gok niet juist was..
     echo 'Helaas! Je hebt geen pogingen meer!'
     echo 'Het random getal was: ' $Random_Getal
 fi
